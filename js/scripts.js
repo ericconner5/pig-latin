a8f43c7a121af8754e0e015fcf3d6cd56bbf92b9
Eric Conner and Hingyi Khong <hingyi@>
2018-08-15 16:33:20 -0700

    add html, js

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,30 @@
-$(document).ready(function() {
-  $("form#latinator").submit(function(event) {
+var vowels = ["a", "e", "i", "o", "u"];
+var consonants = ["b", "c", "d", "f", "g", "h", "j", "k", "l", "m", "n", "p", "q", "r", "s", "t", "v", "w", "x", "y", "z"];
+var qu = ["qu"];
+var ay = ["ay"];
+var way = ["way"];
+var userType = $("#userInput").val()
+
+$(document).ready(function(event) {
+  $("form#latinator").submit(function() {
     event.preventDefault();
-  consonant = [0]
-  
-  var vowels = ["a", "e", "i", "o", "u"];
-  var consonants = ["b", "c", "d", "f", "g", "h", "j", "k", "l", "m", "n", "p", "q", "r", "s", "t", "v", "w", "x", "y", "z"];
-  var qu = ["qu"];
-  var ay = ["ay"];
-  var way = ["way"];
-)}
-}
+    console.log("anything")
+
+    // consonant = 0;
+    // index = 0;
+
+  });
+});
+  //
+  // console.log ("goes through ready section")
+  // // if (userType === vowels[0]) {}
+  // function submit(){
+  //   var str = userInput;
+  //   var res = userInput.charAt(0)
+  //   document.getElementById('demo').innerHTML = res;
+  // }
+  //
+  // alert(#userType);
+  //   ;
+  //
+  // console.log ("user input recieved???")
